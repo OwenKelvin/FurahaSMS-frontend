043d377eebade279f7d842bfd8d0d27fa068a621
Owen Kelvin <otienoowenkelvin@gmail.com>
2019-12-06 17:30:51 +0300

    Moved @angular/cli to dependancies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'furaha-smsT';
+  title = 'Furaha School Management System';
 }
