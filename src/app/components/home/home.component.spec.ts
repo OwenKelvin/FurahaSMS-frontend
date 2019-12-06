ebf2cbbc061347eb641dfcba551287db481bf815
Owen Kelvin <otienoowenkelvin@gmail.com>
2019-12-06 21:50:08 +0300

    Fixed failing tests

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { FullWithCenterComponent } from '../full-with-center/full-with-center.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent, FullWithCenterComponent ]
     })
     .compileComponents();
   }));
--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { FullWithCenterComponent } from '../full-with-center/full-with-center.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +9,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent, FullWithCenterComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
