203b26daf9697e6980856ba679ba5f3644f4389f
Owen Kelvin <53997274+OwenKelvin@users.noreply.github.com>
2019-12-09 11:46:15 +0300

    Revert "Ft login success redirect #170169745"

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -9,31 +9,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginContactAdminComponent } from './login-contact-admin/login-contact-admin.component';
 import { LoginResetComponent } from './login-reset/login-reset.component';
 import { InputComponent } from './input/input.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginModule } from './login/login.module';
-import { ToastComponent } from './toast/toast.component';
-import { LogoutButtonComponent } from './logout-button/logout-button.component';
-import { LayoutComponent } from './layout/layout.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    HomeComponent,
-    FullWithCenterComponent,
-    LoginContactAdminComponent,
-    LoginResetComponent,
-    InputComponent,
-    DashboardComponent,
-    ToastComponent,
-    LogoutButtonComponent,
-    LayoutComponent,
-    HeaderComponent,
-    FooterComponent,
-  ],
-  imports: [CommonModule, ComponentsRoutingModule, ReactiveFormsModule, LoginModule],
-  exports: [LoginComponent, HomeComponent, ToastComponent]
+  declarations: [LoginComponent, HomeComponent, FullWithCenterComponent, LoginContactAdminComponent, LoginResetComponent, InputComponent],
+  imports: [CommonModule, ComponentsRoutingModule, ReactiveFormsModule],
+  exports: [LoginComponent, HomeComponent]
 })
 export class ComponentsModule {}
