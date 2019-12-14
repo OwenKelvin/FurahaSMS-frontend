109ec315ab0a5cafc97622f89859e0d5a8e16d55
Owen Kelvin <otienoowenkelvin@gmail.com>
2019-12-14 21:19:40 +0300

    fixed tests

--- src/app/components/academics-curriculum-units/academics-curriculum-units.component.spec.ts
+++ src/app/components/academics-curriculum-units/academics-curriculum-units.component.spec.ts
@@ -3,6 +3,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AcademicsCurriculumUnitsComponent } from './academics-curriculum-units.component';
 import { Store, StoreModule } from '@ngrx/store';
 import { AppState } from 'src/app/store/reducers';
+import { ViewItemsComponent } from '../view-items/view-items.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ChipsComponent } from '../chips/chips.component';
+import { LoadingBubbleComponent } from '../loading-bubble/loading-bubble.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AcademicsCurriculumUnitsComponent', () => {
   let component: AcademicsCurriculumUnitsComponent;
@@ -11,8 +16,16 @@ describe('AcademicsCurriculumUnitsComponent', () => {
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      imports: [ StoreModule.forRoot({}) ],
-      declarations: [ AcademicsCurriculumUnitsComponent ]
+      imports: [
+        StoreModule.forRoot({}),
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      declarations: [
+        AcademicsCurriculumUnitsComponent,
+        ViewItemsComponent,
+        LoadingBubbleComponent,
+        ChipsComponent]
     });
 
     await TestBed.compileComponents();
