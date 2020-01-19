dc280ff42d97adeea86614bd5d376149067c1682
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-01-19 07:13:15 +0300

    Removed unused imports from components module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ComponentsRoutingModule } from './components-routing.module';
-import { HomeComponent } from './home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToastComponent } from './toast/toast.component';
-import { ErrorModule } from './error/error.module';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { AccordionModule } from 'ngx-bootstrap/accordion';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { AppInputModule } from 'src/app/modules/app-input.module';
 import { AppLayoutModule } from '../modules/app-layout.module';
-import { AppDashboardLinksModule } from '../modules/app-dashboard-links';
-
-
 
 @NgModule({
   declarations: [
@@ -21,15 +9,7 @@ import { AppDashboardLinksModule } from '../modules/app-dashboard-links';
   ],
   imports: [
     CommonModule,
-    NgSelectModule,
-    ComponentsRoutingModule,
-    ReactiveFormsModule,
-    ErrorModule,
-    AccordionModule.forRoot(),
-    TabsModule.forRoot(),
-    AppInputModule,
     AppLayoutModule,
-    AppDashboardLinksModule,
 
   ],
   exports: [
