2d3ac3fb608cdb84cb177375a6b2809ccda7ff1a
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-01-27 06:55:54 +0300

    Removed hard corded links in view students

--- src/app/pages/admissions/students-recently-created/students-recently-created.component.spec.ts
+++ src/app/pages/admissions/students-recently-created/students-recently-created.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StudentsRecentlyCreatedComponent } from './students-recently-created.component';
 import { Store, StoreModule } from '@ngrx/store';
 import { AppState } from 'src/app/store/reducers';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppLoadingBubbleModule } from 'src/app/modules/app-loading-bubble';
 
 describe('StudentRecentlyCreatedComponent', () => {
   let component: StudentsRecentlyCreatedComponent;
@@ -11,7 +14,12 @@ describe('StudentRecentlyCreatedComponent', () => {
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
-      imports: [ StoreModule.forRoot({}) ],
+      imports: [
+        StoreModule.forRoot({}),
+        HttpClientTestingModule,
+        RouterTestingModule,
+        AppLoadingBubbleModule
+      ],
       declarations: [ StudentsRecentlyCreatedComponent ]
     });
 
--- src/app/pages/admissions/students-recently-created/students-recently-created.component.ts
+++ src/app/pages/admissions/students-recently-created/students-recently-created.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromStore from '../../../store/reducers';
+import { Observable } from 'rxjs';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-students-recently-created',
@@ -8,10 +10,15 @@ import * as fromStore from '../../../store/reducers';
   styleUrls: ['./students-recently-created.component.css']
 })
 export class StudentsRecentlyCreatedComponent implements OnInit {
+  students$: Observable<any[]>
 
-  constructor(private store: Store<fromStore.AppState>) { }
+  constructor(
+    private studentsService: StudentService,
+    private store: Store<fromStore.AppState>) { }
 
   ngOnInit() {
+    this.students$ = this.studentsService.getRecentlyCreatedStudents();
+    this.students$.subscribe(item => console.log(item))
   }
 
 }
