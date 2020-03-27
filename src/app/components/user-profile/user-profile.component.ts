8b0c031f6f9b7d734e87eedd4004dbb09187a0ce
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-03-27 15:07:32 +0300

    User can view selected image

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
+  context: any;
 
   constructor() { }
   @Input() title: string;
@@ -14,6 +15,8 @@ export class UserProfileComponent implements OnInit {
   @Input() links: any[];
   @Input() includeProfileId = true;
   editMode: boolean = false;
+
+  photoSrc: any;
 
   ngOnInit() {
   }
@@ -29,4 +32,53 @@ export class UserProfileComponent implements OnInit {
     }
     return [...this.linkBase, link];
   }
+
+  onFileSelected() {
+    const $canvas: any = document.querySelector('#profilePhotoCanvas');
+    this.context = $canvas.getContext('2d');
+    const imageObj = new Image();
+    const $input: any = document.querySelector('#profilePhotoInput');
+    const photoFile: File = (($input as HTMLInputElement).files as FileList)[0];
+    this.photoSrc = URL.createObjectURL(photoFile);
+    imageObj.onload = () => {
+      this.fitImageOn($canvas, imageObj);
+    };
+    imageObj.src = this.photoSrc;
+
+  }
+  
+  fitImageOn(canvas: any, imageObj: any) {
+    const imageAspectRatio = imageObj.width / imageObj.height;
+    const canvasAspectRatio = canvas.width / canvas.height;
+    let renderableHeight, renderableWidth, xStart, yStart;
+
+    // If image's aspect ratio is less than canvas's we fit on height
+    // and place the image centrally along width
+    if (imageAspectRatio < canvasAspectRatio) {
+      renderableHeight = canvas.height;
+      renderableWidth = imageObj.width * (renderableHeight / imageObj.height);
+      xStart = (canvas.width - renderableWidth) / 2;
+      yStart = 0;
+    }
+
+    // If image's aspect ratio is greater than canvas's we fit on width
+    // and place the image centrally along height
+    else if (imageAspectRatio > canvasAspectRatio) {
+      renderableWidth = canvas.width;
+      renderableHeight = imageObj.height * (renderableWidth / imageObj.width);
+      xStart = 0;
+      yStart = (canvas.height - renderableHeight) / 2;
+    }
+
+    // Happy path - keep aspect ratio
+    else {
+      renderableHeight = canvas.height;
+      renderableWidth = canvas.width;
+      xStart = 0;
+      yStart = 0;
+    }
+    this.context.drawImage(imageObj, xStart, yStart, renderableWidth, renderableHeight);
+  };
+
+
 }
