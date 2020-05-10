b9ee28625a5bbc39bfc2abd3255653179bd7f7d0
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-05-10 08:18:50 +0300

    Added animations to scrolling

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -3,11 +3,14 @@ import { Store } from '@ngrx/store';
 import { selectShowMenu } from './../../store/selectors/menu-toggle.selector';
 import { AppState } from './../../store/reducers';
 import { Observable } from 'rxjs';
+import { routerTransition } from './route.animation';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+  styleUrls: ['./layout.component.css'],
+  animations: [routerTransition],
 })
 export class LayoutComponent implements OnInit {
   isMenuOpen$: Observable<boolean>;
@@ -18,6 +21,11 @@ export class LayoutComponent implements OnInit {
 
   ngOnInit() {
     this.isMenuOpen$ = this.store.select(selectShowMenu);
+  }
+  
+  getState(outlet: RouterOutlet) {
+    return outlet.component
+    // return outlet.activatedRouteData.state;
   }
 
 }
