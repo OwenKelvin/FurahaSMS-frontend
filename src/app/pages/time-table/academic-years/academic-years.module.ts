6ddf46c8e8a8d3e2b75f0fa0dd73bdbfe20f865d
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-08-08 21:39:16 +0300

    User can view timetable edit button

--- src/app/pages/time-table/academic-years/academic-years.module.ts
+++ src/app/pages/time-table/academic-years/academic-years.module.ts
@@ -5,11 +5,17 @@ import { AcademicYearsRoutingModule } from './academic-years-routing.module';
 import { TimeTableAcademicYearViewComponent } from './time-table-academic-year-view/time-table-academic-year-view.component';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { TimeTableAcademicYearEditComponent } from './time-table-academic-year-edit/time-table-academic-year-edit.component';
+import { TimetableEditorDirective } from '../directives/timetable-editor.directive';
 
 
 
 @NgModule({
-  declarations: [AcademicYearsComponent, TimeTableAcademicYearViewComponent, TimeTableAcademicYearEditComponent],
+  declarations: [
+    AcademicYearsComponent,
+    TimeTableAcademicYearViewComponent,
+    TimeTableAcademicYearEditComponent,
+    TimetableEditorDirective
+  ],
   imports: [
     CommonModule,
     AcademicYearsRoutingModule,
---
+++ src/app/pages/time-table/directives/timetable-editor.directive.spec.ts
@@ -0,0 +1,8 @@
+import { TimetableEditorDirective } from './timetable-editor.directive';
+
+describe('TimetableEditorDirective', () => {
+  it('should create an instance', () => {
+    const directive = new TimetableEditorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
