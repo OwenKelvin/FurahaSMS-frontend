e71dc9b204b3e82800b0962135dfc12233aa7f04
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-09-16 22:24:43 +0300

    Added content edit and delete buttons

--- src/app/pages/academics/e-learning/e-learning-topic-materials/e-learning-topic-materials.component.ts
+++ src/app/pages/academics/e-learning/e-learning-topic-materials/e-learning-topic-materials.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-e-learning-topic-materials',
   templateUrl: './e-learning-topic-materials.component.html',
   styleUrls: ['./e-learning-topic-materials.component.css']
 })
-export class ELearningTopicMaterialsComponent implements OnInit {
+export class ELearningTopicMaterialsComponent {
   @Input() learningContents: any[];
-  constructor() { }
+  @Input() edit: boolean;
 
-  ngOnInit(): void {
+  constructor() {
   }
+
 
 }
