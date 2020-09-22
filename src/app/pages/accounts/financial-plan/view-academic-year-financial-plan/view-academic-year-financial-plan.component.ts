16ab5d0fb53a04141eb719ef5d24c0f165da5c9d
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-09-22 22:17:43 +0300

    User can view academic year plan

--- src/app/pages/accounts/financial-plan/view-academic-year-financial-plan/view-academic-year-financial-plan.component.ts
+++ src/app/pages/accounts/financial-plan/view-academic-year-financial-plan/view-academic-year-financial-plan.component.ts
@@ -2,9 +2,10 @@ import {Component} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 import {AppState} from 'src/app/store/reducers';
 import {selectPlanForAcademicYearWithId} from '../store/selectors/academic-year-plan.selectors';
-import {Observable} from 'rxjs';
+import {combineLatest, Observable} from 'rxjs';
 import {map, mergeMap, tap} from 'rxjs/operators';
 import {ActivatedRoute} from '@angular/router';
+import {AcademicYearService} from '../../../academics/services/academic-year.service';
 
 @Component({
   selector: 'app-view-academic-year-financial-plan',
@@ -18,7 +19,35 @@ export class ViewAcademicYearFinancialPlanComponent {
   academicYearPlan$: Observable<any> = this.academicYearPlanId$.pipe(
     mergeMap(id => this.store.pipe(select(selectPlanForAcademicYearWithId(id))))
   );
-  constructor(private store: Store<AppState>, private route: ActivatedRoute) {
+
+  semesters$ = this.academicYearPlanId$.pipe(
+    mergeMap(id => this.academicYearService.getSemestersForAcademicYearWithId(id))
+  )
+  academicYear$ = this.academicYearPlan$.pipe(
+    map((plan) => plan?.academicYear)
+  )
+  formattedPlan$: Observable<any[]> = this.academicYearPlan$.pipe(
+    map((plan) => (plan ? plan.financialYearPlan : {})),
+    map(({tuitionFee, otherFees}) =>
+      tuitionFee?.map((fee: any) => ({
+          ...fee,
+          tuitionSemTotals: fee.semTotals,
+          tuitionTotals: fee.total,
+          ...otherFees.find(({classLevelId}: any) => classLevelId === fee.classLevelId),
+        })
+      ).map((item: any) => ({...item, totals: item.total + item.tuitionTotals}))
+    ),
+  );
+
+  v$ = combineLatest([this.academicYear$, this.formattedPlan$, this.semesters$]).pipe(
+    map(([academicYear, financialPlan, semesters]) => ({academicYear, financialPlan, semesters}))
+  )
+
+  constructor(
+    private store: Store<AppState>,
+    private route: ActivatedRoute,
+    private academicYearService: AcademicYearService
+  ) {
   }
 
 }
